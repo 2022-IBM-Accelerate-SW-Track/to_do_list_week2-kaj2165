573c6d3eeb3977327c7973a486ad72ac131c2abe
kaj2165 <kaj2165@columbia.edu>
2022-06-16 16:49:44 -0400

    completed lab two modifications

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
-import React, { Component } from "react";
-import Todos from "../component/todos";
-import AddTodo from "../component/AddTodo";
-import "../pages/Home.css";
-
+import React, { Component } from 'react';
+import './Home.css';
+import AddTodo from '../component/AddTodo.js';
+import Todos from '../component/todos.js'; 
 class Home extends Component {
-  // Create a default state of this component with an empty list of todos.
+  // A default state of this component with an empty list of todos.
   constructor() {
     super();
     this.state = {
-      todos: [],
+      todos:[]
     };
   }
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
@@ -20,9 +19,9 @@ class Home extends Component {
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
+    // An array that contains the current array and the new todo item
     let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
+    // Updates the local state with the new array.
     this.setState({
       todos: new_list,
     });
@@ -31,12 +30,8 @@ class Home extends Component {
     return (
       <div className="Home">
         <h1>Todo's </h1>
-        {/* When passing the AddTodo component, addTodo is a prop that is used in the 
-        AddTodo.js file when handling the submit */}
-        <AddTodo addTodo={this.addTodo} />
-        {/* When returning the Todos component, todos is a prop passed to the todos.js file
-         to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
